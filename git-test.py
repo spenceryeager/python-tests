2dcd6cd01eda84823364a0aed3c7eeaf6c791c8b
Spencer Yeager <yeager.spencer@gmail.com>
2021-08-05 23:31:14 -0700

    Messing around

--- git-test.py
+++ git-test.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3.9
 import matplotlib.pyplot as plt
 import tkinter.messagebox as mb 
+from tkinter import *
 import numpy as np
 
+root = Tk()
+root.withdraw()
+
 # this is a comment
+
+#this is going to test autopep8. Lets do it
 
 mb.showinfo(title="Attention", message="This is a github test. I like VSCodium so far!")
 
