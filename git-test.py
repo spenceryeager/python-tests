cc9012c746d5527858cd3dee5199504d0a4b7b1f
Spencer Yeager <yeager.spencer@gmail.com>
2021-08-05 23:33:24 -0700

    autopep8

--- git-test.py
+++ git-test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.9
 import matplotlib.pyplot as plt
-import tkinter.messagebox as mb 
+import tkinter.messagebox as mb
 from tkinter import *
 import numpy as np
 
@@ -9,9 +9,10 @@ root.withdraw()
 
 # this is a comment
 
-#this is going to test autopep8. Lets do it
+# this is going to test autopep8. Lets do it
 
-mb.showinfo(title="Attention", message="This is a github test. I like VSCodium so far!")
+mb.showinfo(title="Attention",
+            message="This is a github test. I like VSCodium so far!")
 
 x = np.linspace(1, 10, 100)
 y = np.sqrt(x)
