4aae755ecc6369a0f1b6d89fd4cf81c27754df8c
Spencer Yeager <yeager.spencer@gmail.com>
2021-10-07 12:19:00 -0700

    numpy where test

---
+++ numpy_where_test.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def main():
+    workingfile = '/home/spenceryeager/Documents/calculations/derivative_calc/010Vs.csv'
+    data = pd.read_csv(workingfile)
+
+if __name__ == '__main__':
+    main()
